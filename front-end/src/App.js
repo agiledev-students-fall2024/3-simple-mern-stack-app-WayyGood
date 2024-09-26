5e7aa2b8b24b72c783c87de8e6270dd603ced4a9
Nathan Daniel <natedan44@gmail.com>
2024-09-26 09:59:47 -0400

    back end and front end additions

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,8 @@ import MessageStandalone from './MessageStandalone'
 import Home from './Home'
 import Header from './Header'
 import Footer from './Footer'
+import AboutUs from './AboutUs';
+
 
 const App = props => {
   return (
@@ -24,6 +26,9 @@ const App = props => {
               path="/messages/:messageId"
               element={<MessageStandalone />}
             />
+            {/* a route for the About Me page*/}
+            <Route path="/about" element={<AboutUs />} />
+
           </Routes>
         </main>
         <Footer />
